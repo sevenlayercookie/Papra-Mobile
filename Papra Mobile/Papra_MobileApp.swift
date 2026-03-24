da46aa38696a8060dea726b967a9a46d47125ccb
sevenlayercookie <sevenlayercookie@gmail.com>
2026-03-24 14:27:54 -0500

    Initial Commit

---
+++ Papra Mobile/Papra_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Papra_MobileApp.swift
+//  Papra Mobile
+//
+//  Created by Harrison Rose on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Papra_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Papra MobileTests/Papra_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  Papra_MobileTests.swift
+//  Papra MobileTests
+//
+//  Created by Harrison Rose on 3/24/26.
+//
+
+import Testing
+@testable import Papra_Mobile
+
+struct Papra_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
